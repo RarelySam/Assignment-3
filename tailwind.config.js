6787053aa614a2d7f4e17e8fcd990f09d5cb62d1
RarelySam <77665007+RarelySam@users.noreply.github.com>
2024-08-25 21:31:11 +0600

    big big update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./index.html"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        roboto: ["Roboto", "sans-serif"],
+      },
+    },
   },
   plugins: [],
-}
-
+};
