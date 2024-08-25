6c1ae670a9b42e7d309c8ce22cbbb329e120bcec
RarelySam <77665007+RarelySam@users.noreply.github.com>
2024-08-26 01:56:55 +0600

    Last commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,7 @@
 module.exports = {
   content: ["./index.html"],
   theme: {
-    extend: {
-      fontFamily: {
-        roboto: ["Roboto", "sans-serif"],
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
